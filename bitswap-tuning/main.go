4c1aaa930ef4e0c0a24e7e3f9bea91d2eb1a9662
Raúl Kripalani <raul@protocol.ai>
2020-04-22 17:59:06 +0100

    migrate test plans from testground/testground. (#1)
    * migrate bitswap-tuning test plan from testground/testground.
    
    * intake naming plan README.

---
+++ bitswap-tuning/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	test "github.com/ipfs/test-plans/bitswap-tuning/test"
+	"github.com/testground/sdk-go/runtime"
+)
+
+func main() {
+	runtime.Invoke(run)
+}
+
+func run(runenv *runtime.RunEnv) error {
+	switch c := runenv.TestCase; c {
+	case "transfer":
+		return test.Transfer(runenv)
+	case "fuzz":
+		return test.Fuzz(runenv)
+	default:
+		panic("unrecognized test case")
+	}
+}
